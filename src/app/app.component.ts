8317f9a44a265888935262a2c27a6f9f92f574a5
Greicy Huaman <greicyhuaman06@gmail.com>
2024-10-25 04:25:36 -0500

    Registrar Usuario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UsuarioComponent } from './components/usuario/usuario.component';
 
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatMenuModule} from '@angular/material/menu';
+
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, UsuarioComponent],
+  imports: [RouterOutlet, UsuarioComponent, MatMenuModule, MatIconModule, MatToolbarModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'frontTalentTune';
--- src/app/components/usuario/creaeditausuario/creaeditausuario.component.ts
+++ src/app/components/usuario/creaeditausuario/creaeditausuario.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { MatButtonModule } from '@angular/material/button';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Usuario } from '../../../models/Usuario';
+import { UsuarioService } from '../../../services/usuario.service';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @Component({
   selector: 'app-creaeditausuario',
@@ -12,8 +24,56 @@ import { MatButtonModule } from '@angular/material/button';
     MatFormFieldModule,
     MatSelectModule,
     MatButtonModule,
+    ReactiveFormsModule,
+    CommonModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
   ],
   templateUrl: './creaeditausuario.component.html',
   styleUrl: './creaeditausuario.component.css',
 })
-export class CreaeditausuarioComponent {}
+export class CreaeditausuarioComponent implements OnInit {
+  form: FormGroup = new FormGroup({});
+  usuario: Usuario = new Usuario();
+
+  listaGeneros: { value: string; viewValue: string }[] = [
+    { value: 'Femenino', viewValue: 'Femenino' },
+    { value: 'Masculino', viewValue: 'Masculino' },
+  ];
+
+  constructor(
+    private uS: UsuarioService,
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      unombres: ['', Validators.required],
+      ufechaNacimiento: ['', Validators.required],
+      udescripcion: ['', Validators.required],
+      upais: ['', Validators.required],
+      uagencia: ['', Validators.required],
+      usexo: ['', Validators.required],
+      uestudios: ['', Validators.required],
+    });
+  }
+
+  aceptar(): void {
+    if (this.form.valid) {
+      this.usuario.nombres = this.form.value.unombres;
+      this.usuario.sexo = this.form.value.usexo;
+      this.usuario.descripcion = this.form.value.udescripcion;
+      this.usuario.fechaNacimiento = this.form.value.ufechaNacimiento;
+      this.usuario.pais = this.form.value.upais;
+      this.usuario.estudios = this.form.value.uestudios;
+      this.usuario.agencia = this.form.value.uagencia;
+      this.uS.insertar(this.usuario).subscribe(d=>{
+        this.uS.listar().subscribe(d=>{
+          this.uS.setList(d)
+        })
+      })
+    }
+    this.router.navigate(['usuarios'])
+  }
+}
