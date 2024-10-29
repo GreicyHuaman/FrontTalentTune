419efbe47e06717dda082ec8af355b61402cf009
Greicy Huaman <greicyhuaman06@gmail.com>
2024-10-29 12:36:09 -0500

    Resuelto conflicto en creaeditausuario.component.ts

--- src/app/components/usuario/creaeditausuario/creaeditausuario.component.ts
+++ src/app/components/usuario/creaeditausuario/creaeditausuario.component.ts
@@ -16,4 +16,51 @@ import { MatButtonModule } from '@angular/material/button';
   templateUrl: './creaeditausuario.component.html',
   styleUrl: './creaeditausuario.component.css',
 })
-export class CreaeditausuarioComponent {}
+export class CreaeditausuarioComponent implements OnInit {
+  form: FormGroup = new FormGroup({});
+  usuario: Usuario = new Usuario();
+
+  listaGeneros: {
+    value: string;
+    viewValue: string;
+  }[] = [
+    { value: 'Femenino', viewValue: 'Femenino' },
+    { value: 'Masculino', viewValue: 'Masculino' },
+  ];
+
+  constructor(
+    private uS: UsuarioService,
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      unombres: ['', Validators.required],
+      ufechaNacimiento: ['', Validators.required],
+      udescripcion: ['', Validators.required],
+      upais: ['', Validators.required],
+      uagencia: ['', Validators.required],
+      usexo: ['', Validators.required],
+      uestudios: ['', Validators.required],
+    });
+  }
+
+  aceptar(): void {
+    if (this.form.valid) {
+      this.usuario.nombres = this.form.value.unombres;
+      this.usuario.sexo = this.form.value.usexo;
+      this.usuario.descripcion = this.form.value.udescripcion;
+      this.usuario.fechaNacimiento = this.form.value.ufechaNacimiento;
+      this.usuario.pais = this.form.value.upais;
+      this.usuario.estudios = this.form.value.uestudios;
+      this.usuario.agencia = this.form.value.uagencia;
+      this.uS.insertar(this.usuario).subscribe((d) => {
+        this.uS.listar().subscribe((d) => {
+          this.uS.setList(d);
+        });
+      });
+    }
+    this.router.navigate(['usuarios']);
+  }
+}
