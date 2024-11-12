aa134c0da0637b46e4c56bc82b1ae556bd65701f
Greicy Huaman <greicyhuaman06@gmail.com>
2024-11-12 01:02:04 -0500

    merge Valeria into Greicy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,27 +3,12 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterModule, RouterOutlet } from '@angular/router';
-import { BandaComponent } from './components/banda/banda.component';
-import { CategoriaComponent } from './components/categoria/categoria.component';
-import { EventoComponent } from './components/evento/evento.component';
-import { NotificacionComponent } from './components/notificacion/notificacion.component';
-import { RegistroeventoComponent } from './components/registroevento/registroevento.component';
-import { RolComponent } from './components/rol/rol.component';
-import { UsuarioComponent } from './components/usuario/usuario.component';
-import { ContratoComponent } from './components/contrato/contrato.component';
-
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    UsuarioComponent,
-    BandaComponent,
-    CategoriaComponent,
-    NotificacionComponent,
-    RolComponent,
-    ContratoComponent,
     MatToolbarModule,
     MatIconModule,
     MatMenuModule,
